79ae9a2a7d67b07aca65837f3bb9617d2aa98e22
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-20 16:02:45 +0530

    Follow coding guidelines

--- DBT_example.cpp
+++ DBT_example.cpp
@@ -29,74 +29,65 @@ vector< Rect_<int> > faces;
 int main( int argc,char* argv[])
 {
 
-     // The header files inside opencv2/contrib are for programs that have been contributed or are experimental
+     	// The header files inside opencv2/contrib are for programs that have been contributed or are experimental
 
-    // Declare the parameters that are needed for the tracker to run;
-    // All the parameters are mostly the same that you use for haar cascades.
+    	// Declare the parameters that are needed for the tracker to run;
+    	// All the parameters are mostly the same that you use for haar cascades.
 
-    DetectionBasedTracker::Parameters param;
-    param.maxObjectSize = 400;
-    param.maxTrackLifetime = 20;
-    param.minDetectionPeriod = 7;
-    param.minNeighbors = 3;
-    param.minObjectSize = 20;
-    param.scaleFactor = 1.1;
+    	DetectionBasedTracker::Parameters param;
+    	param.maxObjectSize = 400;
+    	param.maxTrackLifetime = 20;
+    	param.minDetectionPeriod = 7;
+    	param.minNeighbors = 3;
+    	param.minObjectSize = 20;
+    	param.scaleFactor = 1.1;
 
-    //The constructer is called with the cascade of choice and the Parameter structure
+    	//The constructer is called with the cascade of choice and the Parameter structure
 
-    DetectionBasedTracker obj = DetectionBasedTracker("haarcascade_frontalface_alt.xml",param);
+    	DetectionBasedTracker obj = DetectionBasedTracker("haarcascade_frontalface_alt.xml",param);
+    	obj.run();
+	VideoCapture cap(0);
+	Mat img,gray;
+	cv::Rect_<int> face_i;
+	cv::namedWindow("Detection Based Tracker",cv::WINDOW_NORMAL);
+	cv::setWindowProperty("Detection Based Tracker", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
 
-    obj.run();
+    	for (;;) {
 
-    VideoCapture cap(0);
+        	cap>>img;
+        	cv::cvtColor(img,gray,CV_RGB2GRAY);
 
-    Mat img,gray;
-    cv::Rect_<int> face_i;
+		 // The class object is run on a gray image.
 
-    cv::namedWindow("Detection Based Tracker",cv::WINDOW_NORMAL);
+        	obj.process(gray);
 
-    cv::setWindowProperty("Detection Based Tracker", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+        	// The results are a vector of Rect that enclose the object being tracked
+        	obj.getObjects(faces);
 
-    for(;;)
-    {
+       		// if(faces.size() == 0) obj.resetTracking();
 
-        cap>>img;
-        cv::cvtColor(img,gray,CV_RGB2GRAY);
+        	for (int i = 0; i < faces.size(); i++) {
+            	face_i = faces[i];
 
+            	// Make a rectangle around the detected object
 
-        // The class object is run on a gray image.
+            	rectangle(img, face_i, CV_RGB(0, 255,0), 3);
 
-        obj.process(gray);
+            	string box_text = format("Tracked Area");
 
-        // The results are a vector of Rect that enclose the object being tracked
-        obj.getObjects(faces);
+            	int pos_x = std::max(face_i.tl().x - 10, 0);
+            	int pos_y = std::max(face_i.tl().y - 10, 0);
+            	// And now put it into the image:
+            	putText(img, box_text, Point(pos_x, pos_y), FONT_HERSHEY_SIMPLEX, 1.0, CV_RGB(0,255,0), 2.0);
 
-       // if(faces.size() == 0) obj.resetTracking();
+         	}
 
-        for(int i = 0; i < faces.size(); i++)
-        {
-            face_i = faces[i];
+    		cv::imshow("Detection Based Tracker",img);      // Show the results.
+	
+		if(cv::waitKey(33) == 27) break;
 
-            // Make a rectangle around the detected object
+    	}
 
-            rectangle(img, face_i, CV_RGB(0, 255,0), 3);
-
-            string box_text = format("Tracked Area");
-
-            int pos_x = std::max(face_i.tl().x - 10, 0);
-            int pos_y = std::max(face_i.tl().y - 10, 0);
-            // And now put it into the image:
-            putText(img, box_text, Point(pos_x, pos_y), FONT_HERSHEY_SIMPLEX, 1.0, CV_RGB(0,255,0), 2.0);
-
-         }
-
-    cv::imshow("Detection Based Tracker",img);      // Show the results.
-
-    if(cv::waitKey(33) == 27) break;
-
-    }
-
-    obj.stop();
-
+    	obj.stop();
 	return 0;
 }
